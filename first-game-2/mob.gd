b670844b8ba1e3cde15545a56bfcc277f500dfce
PC-LENOVO\Admin <297180416@qq.com>
2026-03-04 00:08:55 +0800

    feat: 初始化Godot 2D游戏项目并添加基础资源
    添加Godot 2D游戏项目"first game2"的基础文件，包括：
    - 项目配置文件(project.godot)和编辑器设置
    - 玩家(Player)和敌人(Mob)场景及脚本
    - Dodge the Creeps教程的完整资源包(精灵图、音效、字体)
    - 版本控制配置文件(.gitignore, .gitattributes)
    - 游戏图标和UID文件
    
    此提交为后续的2D游戏开发建立完整项目结构。

---
+++ first-game-2/mob.gd
@@ -0,0 +1,6 @@
+extends RigidBody2D
+
+func _ready():
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.play()
